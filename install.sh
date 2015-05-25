d7cc44abb5365bb1e734d0b6d76829c4bdd7c5d3
Hugh Mandeville <hughmandeville@yahoo.com>
2015-05-25 15:24:39 -0400

    Added comment.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# Copy files to the web server.
+# Copy files to the web server (50.62.37.1).
 scp code.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
 scp index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
 scp style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
