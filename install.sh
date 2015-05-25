0ac0d2d41e6d47dd53e9e785627b0b97ede3d3dd
Hugh Mandeville <hughmandeville@yahoo.com>
2015-05-25 16:04:05 -0400

    Install more files.

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 # Copy files to the web server (50.62.37.1).
 scp code.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
+scp connect.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
+scp executive_bios.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
+scp general_bios.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
 scp index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
 scp style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
+scp team.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
+scp visual_bios.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
 #scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
 
 
