f795027020d3113f5f21853239339427d175adcd
Hugh Mandeville <hughmandeville@yahoo.com>
2015-05-25 14:26:06 -0400

    Installs files to server.

---
+++ install.sh
@@ -0,0 +1,6 @@
+# Copy files to the web server.
+scp index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
+scp style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
+#scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
+
+
