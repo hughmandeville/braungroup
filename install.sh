2f4a33e842be9ecb73cba24699c47f2c2d3466de
Hugh Mandeville <hughmandeville@yahoo.com>
2015-06-01 00:17:31 -0400

    Copy png files.

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ scp style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338
 scp team.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
 scp visual_bios.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
 scp images/*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
-#scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
+scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
 
