fbef8ced7a95f3b291b8684f505f6f783e294d9f
Hugh Mandeville <hubert.mandeville@nytimes.com>
2015-08-30 15:45:42 -0400

    add missing.html

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ scp book_slider.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/94
 scp code.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp executive_memoirs.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp general_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp missing.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 #scp robots.txt rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 #scp sitemap.xml rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
