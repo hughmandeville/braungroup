d11c6c8778061e67b516f9f5ee4fc1dbc264b7d3
Hugh Mandeville <hubert.mandeville@nytimes.com>
2015-08-30 11:50:18 -0400

    Updated intall script.

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 # Copy files to the web server (50.62.37.1).
-scp code.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp connect.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp executive_bios.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp general_bios.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp general_bios_desc.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp executive_bios_desc.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp book_reviews.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp venture_dev.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp team.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp visual_bios.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/
-scp images/*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
-scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
+#scp .htaccess rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp book_slider.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp code.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp executive_memoirs.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp general_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp team.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp venture_development.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp visual_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp images/*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
+#scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/new/images/
