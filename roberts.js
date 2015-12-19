f1255d49b1235c9d028f16811fb6dc4db3059062
Hugh Mandeville <hubert.mandeville@nytimes.com>
2015-12-19 16:54:42 -0500

    Added close button.

--- roberts.js
+++ roberts.js
@@ -1,6 +1,6 @@
 $(function() {
     $("area").click(function(e){
-        $("#module_endnote").html($(this).data("endnote"));        
+        $("#module_endnote_text").html($(this).data("endnote"));        
         $("#module_endnote").css("top", e.pageY - 40);
         $("#module_endnote").css("left", e.pageX + 40);        
         $("#module_endnote").show();
