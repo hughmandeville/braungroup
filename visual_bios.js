a31c839ee7c23275cdfa3e003687e2024d10c91f
Hugh Mandeville <hubert.mandeville@nytimes.com>
2016-01-18 18:16:11 -0500

    Made visual cartoon animation smaller on ipad.

--- visual_bios.js
+++ visual_bios.js
@@ -5,42 +5,82 @@ $(function() {
 
 function animate_visual() {
     setTimeout(function() {
+        var bg_size = "600px";
+        var bg_x = "5px";
+        var bg_y = "0px";
+        if ($("#visual").width() == "218") {
+            bg_size = "436px";
+            bg_x = "2px";
+            bg_y = "0px";            
+        }        
         $("#visual").animate({
-            "background-size": "600px",
-            "background-position-x": "5px",
-            "background-position-y": "0px"
+            "background-size": bg_size,
+            "background-position-x": bg_x,
+            "background-position-y": bg_y
         }, 1000, function() {
         });
     }, 4000);
     setTimeout(function() {
+        var bg_size = "600px";
+        var bg_x = "-300px";
+        var bg_y = "0px";
+        if ($("#visual").width() == "218") {
+            bg_size = "436px";
+            bg_x = "-218px";
+            bg_y = "0px";            
+        }        
         $("#visual").animate({
-            "background-size": "600px",
-            "background-position-x": "-300px",
-            "background-position-y": "0px"
+            "background-size": bg_size,
+            "background-position-x": bg_x,
+            "background-position-y": bg_y
         }, 1000, function() {
         });
     }, 8000);
     setTimeout(function() {
+        var bg_size = "600px";
+        var bg_x = "5px";
+        var bg_y = "-394px";
+        if ($("#visual").width() == "218") {
+            bg_size = "436px";
+            bg_x = "2px";
+            bg_y = "-286px";            
+        }        
         $("#visual").animate({
-            "background-size": "600px",
-            "background-position-x": "5px",
-            "background-position-y": "-394px"
+            "background-size": bg_size,
+            "background-position-x": bg_x,
+            "background-position-y": bg_y
         }, 1000, function() {
         });
     }, 12000);
     setTimeout(function() {
+        var bg_size = "600px";
+        var bg_x = "-300px";
+        var bg_y = "-394px";
+        if ($("#visual").width() == "218") {
+            bg_size = "436px";
+            bg_x = "-218px";
+            bg_y = "-286px";            
+        }        
         $("#visual").animate({
-            "background-size": "600px",
-            "background-position-x": "-300px",
-            "background-position-y": "-394px"
+            "background-size": bg_size,
+            "background-position-x": bg_x,
+            "background-position-y": bg_y
         }, 1000, function() {
         });
     }, 16000);
     setTimeout(function() {
+        var bg_size = "300px";
+        var bg_x = "0px";
+        var bg_y = "0px";
+        if ($("#visual").width() == "218") {
+            bg_size = "218px";
+            bg_x = "0px";
+            bg_y = "0px";            
+        }        
         $("#visual").animate({
-            "background-size": "300px",
-            "background-position-x": "0px",
-            "background-position-y": "0px"
+            "background-size": bg_size,
+            "background-position-x": bg_x,
+            "background-position-y": bg_y
         }, 1000, function() {
         });
     }, 20000);
