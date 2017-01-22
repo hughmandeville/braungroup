b1dcfdbc696c223bb11a69aa4e94311b694adb83
admin <admin@Macbook-Air-JPK.local>
2017-01-22 14:22:03 -0500

    Updated file to include new images

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ scp general_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/con
 scp missing.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 #scp robots.txt rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-#scp sitemap.xml rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp sitemap.xml rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp team.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp venture_development.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp visual_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 scp visual_bios.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-#scp images/*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
-#scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
+scp images/*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
+scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
