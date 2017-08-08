d71f72646e64296dc2d7f4b7b41d9908436e796f
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2017-08-07 21:32:20 -0400

    addedsome lines

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 # Copy files to the web server (50.62.37.1).
 #scp .htaccess rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp book_slider.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp code.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp executive_memoirs.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp executive_speechwriting.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp general_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp missing.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp book_slider.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp code.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss executive_memoirs.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss executive_speechwriting.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss general_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp missing.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss index.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
 #scp robots.txt rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp sitemap.xml rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp team.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp venture_development.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp visual_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp visual_bios.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
-scp images/*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
-scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
+#scp sitemap.xml rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss style.css rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss team.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss venture_development.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+scp -oHostKeyAlgorithms=+ssh-dss visual_biographies.html rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss visual_bios.js rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/
+#scp -oHostKeyAlgorithms=+ssh-dss images/poster*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
+#scp -oHostKeyAlgorithms=+ssh-dss images/walton*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
+#scp -oHostKeyAlgorithms=+ssh-dss images/wainwright*jpg rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
+
+#scp images/*png rebeccabraun@braungroupllc.com:/var/chroot/home/content/38/9497338/html/images/
